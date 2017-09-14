d547cdbbb5e36216c662fd7a8b6f276f4ac31afe
Rémi Bastide <Remi.Bastide@irit.fr>
2017-09-14 10:36:49 +0200

    Version initiale

---
+++ src/main/java/example/Inversor.java
@@ -0,0 +1,20 @@
+package example;
+
+public class Inversor {  
+    /**
+     * @param s la chaine à inverser (non nulle)
+     * @return l'inverse de s
+     */
+    public String inverse(String s) {
+        if (s == null) 
+            throw new IllegalArgumentException("Argument must not be null");
+        
+        // Cette implémentation est très inefficace, pourquoi ?
+        String result = "";
+        for (int i = 0; i < s.length(); i++ ) {
+            result = s.charAt(i) + result;
+        }
+        return result;
+    }
+    
+}
---
+++ src/main/java/example/PalindromeCheker.java
@@ -0,0 +1,18 @@
+package example;
+
+public class PalindromeCheker {
+
+	// La dépendance est créée dans la classe
+	private final Inversor inversor = new Inversor();
+
+	/**
+	 * Détermine si une chaine est un palindrome
+	 *
+	 * @param s la chaine à tester (non nulle)
+	 * @return vrai si s est un palindrome, faux sinon
+	 */
+	public boolean estPalindrome(String s) {
+		return inversor.inverse(s).equals(s);
+	}
+
+}
